0df00c854109804e9befbc79b5fc34dbf4292926
Kaue Santana <kauesantana2077@gmail.com>
2025-06-23 18:56:06 +0800

    Practicing more JS

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var spd = 70
+console.log(`Your car's speed is ${spd}km/h`)
+if (spd > 60) { // Simple condition
+    console.log('You have exceeded the speed limit, FINED!')
+}
+console.log('Always drive with seat belt')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var country = 'Brazil'
+console.log(`Living in ${country}`)
+if (country == 'Brazil') {
+    console.log('You are Brazillian')
+} else {
+    console.log('You are Foreigner')
+}
