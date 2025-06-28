97afce5a6e9789d0ca3eec81907ff751aab3828a
Kaue Santana <kauesantana2077@gmail.com>
2025-06-28 21:18:16 +0800

    Studying repetition structure

---
+++ aula013/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Pace ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+var c = 1
+while (c <= 6){
+    console.log(`Pace ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+/*
+console.log('All right?')
+console.log('All right?')
+console.log('All right?')
+console.log('All right?')
+console.log('All right?')
+console.log('All right?')
+*/
