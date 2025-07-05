d98410dca4facfc02eff9404f44eda9af7e222e9
Kaue Santana <kauesantana2077@gmail.com>
2025-07-05 09:18:32 +0800

    Multiplication Table created
    Also added css button just to practice, but it looks ugly

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,1 +1,17 @@
+function generate(){
+    let number = document.querySelector('input#txtNumber')
+    let res = document.querySelector('textarea#boxRes')
+    
+    if(number.value.length == 0) {
+        alert('Please, insert a valid number')
+    } else {
+        let trueNumber = Number(number.value)
+        res.innerHTML = ''
+        
+    for(c = 1; c <= 10; c++) {
+        let times = c*trueNumber
 
+        res.innerHTML += `${trueNumber} x ${c} = ${times} \n`
+    }
+    }
+}
