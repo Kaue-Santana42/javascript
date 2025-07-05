f37d60b0e2e8d46ecbc305b6dfcd8866b836bc77
Kaue Santana <kauesantana2077@gmail.com>
2025-07-05 09:46:52 +0800

    Select table added

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,17 +1,32 @@
 function generate(){
     let number = document.querySelector('input#txtNumber')
     let res = document.querySelector('textarea#boxRes')
+    let tab = document.querySelector('select#seltab')
     
     if(number.value.length == 0) {
         alert('Please, insert a valid number')
     } else {
         let trueNumber = Number(number.value)
+        let c = 1
         res.innerHTML = ''
+        tab.innerHTML = ''
         
-    for(c = 1; c <= 10; c++) {
+        
+    for(; c <= 10; c++) {
         let times = c*trueNumber
 
         res.innerHTML += `${trueNumber} x ${c} = ${times} \n`
+    }
+    
+    c = 1
+
+    while(c <= 10) {
+        let item = document.createElement('option')
+        item.value = `tab${c}` // Each option will receive a value
+
+        item.text = `${trueNumber} x ${c} = ${trueNumber*c}`
+        tab.appendChild(item) // Will add an element/text as a child of tab
+        c++
     }
     }
 }
