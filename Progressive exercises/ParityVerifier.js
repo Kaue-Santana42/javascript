e4c81a5e907ba43727931419e33036db3b57e13f
Kaue Santana <kauesantana2077@gmail.com>
2026-01-30 15:10:31 +0800

    First exercise, avoiding user input mistakes

---
+++ Progressive exercises/ParityVerifier.js
@@ -0,0 +1,22 @@
+console.log('='.repeat(15));
+console.log('Parity Verifier');
+console.log('='.repeat(15));
+
+do {
+    var inputNumber = prompt("Write a number: ");
+
+    if (isFinite(inputNumber) && inputNumber.trim() !== "") { // Check if it is a number and without blank input
+        parityVerifier(inputNumber);
+    } else {
+        console.log("Please, type a number and without space");
+    }
+   
+} while (isFinite(inputNumber) == false || (inputNumber.trim() !== "") == false); // Code continues while user gives wrong inputs
+
+ function parityVerifier(number) { // Check if a number is odd or even using the remainder of the division
+        if (number % 2 == 0) {
+            console.log(`The ${number} is Odd`);
+        } else {
+            console.log(`The ${number} is even`);
+        }
+    }
