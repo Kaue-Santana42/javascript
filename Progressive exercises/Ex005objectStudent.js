f5446c0ea68e70076df21b6095b76323844ca725
Kaue Santana <kauesantana2077@gmail.com>
2026-02-04 12:44:53 +0800

    ex005 finished
    I did while I was searching how it works, but I check ways to improve, improving how the object is used and how an user can add more objects

--- Progressive exercises/Ex005objectStudent.js
+++ Progressive exercises/Ex005objectStudent.js
@@ -7,13 +7,18 @@ const showStudentInfo = (studentName, studentAge, studentState) => console.log(`
 
 // Checks if the name's form was canceled, or if it was left blank, or if a number was typed
 const checkName = (inputName) => inputName !== null && inputName.trim() !== "" && !isFinite(inputName);
+// Checks if the age's form was canceled, left blank, and if it is a number
+const checkAge = (inputAge) => inputAge !== null && inputAge.trim() !== "" && isFinite(inputAge);
+// Check if is registered, return true if yes, false if not
+const checkRegisteredAnswer = (inputRegistered) => inputRegistered === "yes" ? true : false;
 
 
-// Main Code
-var student = new Object();
+// -- Main Code --
+let student = new Object();
 let isNameValid = false;
+let isAgeValid = false;
 
-
+// Check if name is valid
 do {
     student.name = prompt("Enter the student's name: ");
     isNameValid = checkName(student.name);
@@ -23,7 +28,37 @@ do {
     }
 } while(!isNameValid)
 
-student.age = prompt("Enter the student's age: ");
-student.isRegistered = prompt("Is the student registered? (yes or no): ");
+// Check if age is valid
+do {
+    student.age = prompt("Enter the student's age: ");
+    isAgeValid = checkAge(student.age);
+
+    if (!isAgeValid) {
+        console.log("Please, enter only numbers for age (this form can not be blank)")
+    }
+} while (!isAgeValid);
+
+// Check if answer of register is valid
+let isAnswerValid = false;
+
+do {
+    let registeredAnswer = prompt("Is the student registered? (yes or no): ");
+
+    if (registeredAnswer === null || registeredAnswer.trim() === "") { // Check if the form is empty or if it was canceled
+        console.log("Please, write strictly 'yes' or 'no' (this form can not be blank)");
+        continue;
+    }
+
+    registeredAnswer = registeredAnswer.toLowerCase().trim();
+
+    if (registeredAnswer === "no" || registeredAnswer === "yes") {
+        student.isRegistered = checkRegisteredAnswer(registeredAnswer); // return true if is registered
+        isAnswerValid = true;
+    } else {
+        console.log("Please, write strictly 'yes' or 'no'"); // Continue until provide answer yes or no
+    }
+
+} while(!isAnswerValid);
 
 showStudentInfo(student.name, student.age, student.isRegistered);
+
